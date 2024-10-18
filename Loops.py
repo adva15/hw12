0cf3c9de94c0ef2cac36b154328f099234a4f512
adva15 <aadva5319@gmail.com>
2024-10-18 14:57:08 +0300

    Add files via upload

--- Loops.py
+++ Loops.py
@@ -3,6 +3,7 @@ natural_num_n:int=int(input("natural number n:"))
 for _ in range(natural_num_n, 0 ,-1):
     print(_," ",end="")
 print()
+
 #7
 num1:int=int(input("number 1:"))
 num2:int=int(input("number 2:"))
@@ -13,9 +14,16 @@ for numbers in range(num1-1,num2):
         print("",numbers,end="")
 print()
 
-for numbers in range(num1,):
-    if numbers % 3 or  numbers % 5:
-        numbers+=1
-        print("",numbers,end="")
 #8
+num_n:int=int(input("positive number n:"))
 
+for number in range(num_n+1):
+    if number % 3==0 or number % 5==0:
+        num_n = number // 3 or number // 5
+        print("",number,end="")
+#9
+num_n7:int=int(input("number n*7:"))
+
+for number in range(num_n7+1):
+    if number %7==0:
+        print("",number,end="")
